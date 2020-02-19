0bfb9774720b202ed344603bed3a4a7be186a360
shiwei.fu <shiwei.fu@thoughtworks.com>
2020-02-19 11:28:31 +0800

    del no use annotation

--- src/test/java/cc/xpbootcamp/warmup/cashier/OrderTest.groovy
+++ src/test/java/cc/xpbootcamp/warmup/cashier/OrderTest.groovy
@@ -107,41 +107,4 @@ class OrderTest extends Specification {
         resultLine[9] == "折扣:\t1.43"
         resultLine[10] == "总价:\t70.07"
     }
-
-
-//    @Test
-//    void should_print_customer_information_on_order() {
-//        Order order = new Order("Mr X", "Chicago, 60601", LocalDate.now(), new ArrayList<LineItem>());
-//        OrderReceipt receipt = new OrderReceipt(order);
-//        String output = receipt.printReceipt();
-//        final String[] resultLine = output.split(Separator.LINE_BREAK.getValue());
-//        assertThat(resultLine.length).isEqualTo(7);
-//        assertThat(resultLine[0]).isEqualTo("====老王超市，值得信赖====");
-//    }
-//
-//    @Test
-//    void should_print_2020_2_17__monday_when_print_receipt_given_that_date() {
-//        Order order = new Order("Mr X", "Chicago, 60601", LocalDate.parse("2020-02-17"), new ArrayList<LineItem>());
-//        OrderReceipt receipt = new OrderReceipt(order);
-//        String output = receipt.printReceipt();
-//        assertThat(output).contains("====老王超市，值得信赖====");
-//        assertThat(output).contains("2020年2月17日，星期一");
-//    }
-//
-//    @Test
-//    public void should_print_line_item_and_sales_tax_information() {
-//        List<LineItem> lineItems = new ArrayList<LineItem>() {{
-//            add(new LineItem("milk", 10.0, 2));
-//            add(new LineItem("biscuits", 5.0, 5));
-//            add(new LineItem("chocolate", 20.0, 1));
-//        }};
-//        OrderReceipt receipt = new OrderReceipt(new Order(null, null, LocalDate.now(), lineItems));
-//
-//        String output = receipt.printReceipt();
-//        assertThat(output).contains("milk\t10.0\t2\t20.0");
-//        assertThat(output).contains("biscuits\t5.0\t5\t25.0");
-//        assertThat(output).contains("chocolate\t20.0\t1\t20.0");
-////        assertThat(output).contains("Sales Tax\t6.5");
-////        assertThat(output).contains("Total Amount\t71.5");
-//    }
 }
