3ccce5c3b3f93a7c572d87a800e80f6452cc9ffb
shiwei.fu <shiwei.fu@thoughtworks.com>
2020-02-20 14:58:49 +0800

    :recycle: Refactoring code. The division of responsibilities was redrawn

--- src/main/java/cc/xpbootcamp/warmup/cashier/LineItem.java
+++ src/main/java/cc/xpbootcamp/warmup/cashier/LineItem.java
@@ -3,10 +3,8 @@ package cc.xpbootcamp.warmup.cashier;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Value;
 
 @Builder
-@Value(staticConstructor = "of")
 @AllArgsConstructor
 @Getter
 public class LineItem {
@@ -18,9 +16,6 @@ public class LineItem {
   double totalAmount() {
     return price * quantity;
   }
-
-
-
   double getSalesTax() {
     return totalAmount() * .10;
   }
